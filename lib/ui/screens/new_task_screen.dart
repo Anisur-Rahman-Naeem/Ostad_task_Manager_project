0e6a1e1e18c84ae77fcf7fa1c5d3c23619fe8c31
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2024-10-11 23:51:37 +0600

    Summary added

--- lib/ui/screens/new_task_screen.dart
+++ lib/ui/screens/new_task_screen.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/add_new_task_screen.dart';
+
+import '../widgets/task_summary_card.dart';
 
 class NewTaskScreen extends StatelessWidget {
   const NewTaskScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('New'),
+    return Scaffold(
+      body: Column(
+        children: [
+          _buildSummarySection(),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddNewTaskScreen(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
     );
   }
+
+  Widget _buildSummarySection() {
+    return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                TaskSummaryCard(title: 'New', count: 9,
+                ),
+                TaskSummaryCard(title: 'Completed', count: 9,
+                ),
+                TaskSummaryCard(title: 'Cancelled', count: 9,
+                ),
+                TaskSummaryCard(title: 'Progress', count: 9,
+                ),
+              ],
+            ),
+          ),
+        );
+  }
 }
+
