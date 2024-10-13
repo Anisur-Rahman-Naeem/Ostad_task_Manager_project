d34280fe0555f5294a6a89f4bdf1e830849e5d80
Anisur Rahman Naeem <anisurrahmannaeem84@gmail.com>
2024-10-13 18:35:00 +0600

    api integrated in sign Up screen

--- lib/data/services/network_caller.dart
+++ lib/data/services/network_caller.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart';
 import 'package:task_manager/data/models/network_response.dart';
 
 class NetworkCaller {
-  static Future<NetworkResponse> getRequest(String url) async {
+  static Future<NetworkResponse> getRequest({required String url}) async {
     try {
       Uri uri = Uri.parse(url);
       debugPrint(url);
@@ -28,13 +28,12 @@ class NetworkCaller {
         isSuccess: false,
         statusCode: -1,
         errorMessage: e.toString(),
-
       );
     }
   }
 
   static Future<NetworkResponse> postRequest(
-      String url, Map<String, dynamic>? body) async {
+      {required String url, Map<String, dynamic>? body}) async {
     try {
       Uri uri = Uri.parse(url);
       debugPrint(url);
